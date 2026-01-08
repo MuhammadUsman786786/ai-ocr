0f520167b07fd2effcc98d15b020c7550587f9c1
MuhammadUsman <m.usmanqau786@gmail.com>
2026-01-09 00:17:17 +0500

    download model path changes

--- download.py
+++ download.py
@@ -3,6 +3,7 @@ from llama_cpp import Llama
 llm = Llama.from_pretrained(
     repo_id="TheBloke/Llama-2-7B-Chat-GGUF",
     filename="llama-2-7b-chat.Q4_K_M.gguf",
+    cache_dir="../models",
     n_ctx=4096,
     n_gpu_layers=-1,
 )
